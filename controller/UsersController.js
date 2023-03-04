9ef7fa6d88c36484fbd2f67324b2d4cb7822432a
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2023-03-04 09:37:23 +0100

    Add User auth and complete other things

--- controller/UsersController.js
+++ controller/UsersController.js
@@ -1,7 +1,9 @@
 import crypto from 'crypto';
+import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
-function hashPassword(password) {
+export function hashPassword(password) {
   const sha1 = crypto.createHash('sha1');
   sha1.update(password);
   return sha1.digest('hex');
@@ -18,6 +20,19 @@ class UsersControllers {
     const hashedPass = hashPassword(password);
     const newUser = await dbClient.db.collection('users').insertOne({ email, password: hashedPass });
     return res.status(200).send({ id: newUser.insertedId, email: newUser.ops[0].email });
+  }
+
+  async getMe(req, res) {
+    const tokenHeader = req.headers['x-token'];
+    // Get the user id from redis store using token as key
+    const userId = await redisClient.get(`auth_${tokenHeader}`);
+    console.log(userId);
+    // Get the user from the database using the user id
+    const user = await dbClient.db.collection('users').findOne({ _id: new ObjectId(userId) });
+    // res.status(401).send({ error: 'Unauthorized' }); // if user not found
+    if (!user) return res.status(401).send({ error: 'Unauthorized' });
+    // Return the user
+    return res.status(200).send(user);
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import appController from '../controller/AppController';
 import userController from '../controller/UsersController';
+import authController from '../controller/AuthController';
 
 const router = express.Router();
 
+// Get db status
 router.get('/status', (req, res) => appController.getStatus(req, res));
-
+// Get db and redis stats
 router.get('/stats', (req, res) => appController.getStats(req, res));
-
+// Create new user
 router.post('/users', (req, res) => userController.postNew(req, res));
+// Get user by token
+router.get('/users/me', (req, res) => userController.getMe(req, res));
+// Connect user and create new session
+router.get('/connect', (req, res) => authController.getConnect(req, res));
+// Disconnect user and remove session
+router.get('/disconnect', (req, res) => authController.getDisconnect(req, res));
+
 export default router;
